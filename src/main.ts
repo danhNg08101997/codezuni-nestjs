294de0b26b8ec83c9241deba053bac248bcbada1
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-05-05 01:10:40 +0700

    add validation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigAppService } from './config/app/config.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Thực hiện validation cho toàn bộ endpoint
+  app.useGlobalPipes(new ValidationPipe());
+  //---------------------------------------
   const configApp: ConfigAppService = app.get(ConfigAppService)
-
   const config = new DocumentBuilder()
     .setTitle('Monday API')
     .setDescription('Trang API cho dự án Monday')
